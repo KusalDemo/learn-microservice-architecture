c03077b88d3903eabbb5d1d9217c8ba845005fd1
KusalDemo <kusalgunasekara2002@gmail.com>
2025-06-29 09:37:09 +0530

    Added Auditable base class for JPA timestamp auditing

---
+++ common/src/main/java/com/simplehrms/common/Auditable.java
@@ -0,0 +1,27 @@
+package com.simplehrms.common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime updatedAt;
+}
