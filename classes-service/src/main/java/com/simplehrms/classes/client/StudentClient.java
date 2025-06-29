bbb907f87de75f4622295919bcf09b35f05544d0
KusalDemo <kusalgunasekara2002@gmail.com>
2025-06-29 11:33:53 +0530

    Fixed RestClient setup by moving bean to separate config to resolve circular dependency

--- classes-service/src/main/java/com/simplehrms/classes/client/StudentClient.java
+++ classes-service/src/main/java/com/simplehrms/classes/client/StudentClient.java
@@ -2,8 +2,6 @@ package com.simplehrms.classes.client;
 
 import com.simplehrms.classes.dto.StudentResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -18,11 +16,5 @@ public class StudentClient {
                 .uri("http://students-service/api/students/" + id)
                 .retrieve()
                 .body(StudentResponse.class);
-    }
-
-    @Bean
-    @LoadBalanced
-    public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
     }
 }
